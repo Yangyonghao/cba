f0c04c7968cf4eac7e1fcb6d3e5210f25bc7aaff
yangyonghao <yangyonghao@yhxbank.com>
2016-06-23 13:25:38 +0800

    thinkphp开发，自己项目的第一天20160623

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div>前台页面</div>
+</body>
+</html>
